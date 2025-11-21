55cb55445910208ff2bcaa913092c4aeb8f5e611
MaShouo <1324062363@qq.com>
2025-11-21 20:05:11 +0900

    1

--- ProtocolPkg/Protocol.c
+++ ProtocolPkg/Protocol.c
@@ -1,11 +1,12 @@
 /*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Author: MaShouo 1324062363@qq.com
  * @Date: 2025-11-13 17:33:10
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-11-16 22:18:58
+ * @LastEditors: MaShouo 1324062363@qq.com
+ * @LastEditTime: 2025-11-21 20:03:06
  * @FilePath: /edk2/ProtocolPkg/Protocol.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+
 #include <Uefi.h>
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
@@ -51,6 +52,35 @@ UefiMain(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     if(EFI_ERROR(Status))
     {
         Print(L"OpenProtocol failed: %r\n", Status);
+        return Status;
+    }
+
+    UINTN SizeOfInfo = 0;
+    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *Info;
+    for(UINTN i = 0; i < Gop->Mode->MaxMode; i++)
+    {
+        Status = Gop->QueryMode(
+        Gop,
+        i,
+        &SizeOfInfo,
+        &Info
+        );
+
+        if(EFI_ERROR(Status))
+        {
+            Print(L"QueryMode %d failed: %r\n", i, Status);
+            return Status;
+        }
+        Print(L"Mode %d, H = %d, V=%d.\n",
+             i,
+             Info->HorizontalResolution,
+             Info->VerticalResolution);
+    }
+
+    Status = Gop -> SetMode(Gop, 22);
+    if(EFI_ERROR(Status))
+    {
+        Print(L"SetMode failed: %r\n", Status);
         return Status;
     }
 
